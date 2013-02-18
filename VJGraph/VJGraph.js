e5ff9b048f6e75787e8933dd10aa53ee86afba47
unikys <sungihk.yang@kt.com>
2013-02-18 18:52:41 +0900

    Sung-ihk Yang: First version to render graphs

--- VJGraph/VJGraph.js
+++ VJGraph/VJGraph.js
@@ -24,14 +24,70 @@
  */
 
 (function (doc, win, VJ) {
-	var __ = document.querySelector,
-		_ = document.getElementById,
-		_create = document.createElement,
+	var __ = function (q) { return doc.querySelector(q);},
+		_ = function (id) { return doc.getElementById(id);},
+		_create = function (tag) {return doc.createElement(tag);},
 		_objGraphs = {},
-		_DEFAULT_COLORS = ["red", "blue", "green"];
+		excanvas, isLoadingExCanvas = false, pendingProcess, i;
 
 	if (!VJ) {
 		VJ = {};
+	}
+
+	/**
+	 *	If canvas is not supported load excanvas
+	 */
+	if (!(document.createElement("canvas").getContext)) {
+		excanvas = document.createElement("script");
+		isLoadingExCanvas = true;
+		pendingProcess = [];
+		excanvas.onload = function () {
+			isLoadingExCanvas = false;
+			console.log("!");
+		};
+		excanvas.src = "https://explorercanvas.googlecode.com/svn/trunk/silverlight/excanvas.js";
+		document.body.appendChild(excanvas);
+
+	}
+
+	/**
+	 *	Call this function with css.call(dom, stylename)
+	 */
+	function css(styleName, value)	{
+		styleName.replace(/-([a-z])/g, function (g) {
+			return g[1].toUpperCase();
+		});
+		if (value !== undefined) {
+			this.style[styleName] = value;
+			return this;
+		} else {
+			if (this.currentStyle) {
+				return this.currentStyle[styleName];
+			} else if (this.getComputedStyle) {
+				return document.defaultView.getComputedStyle(this)[styleName];
+			} else {
+				return this.style[styleName];
+			}
+		}
+	}
+
+
+
+	function setDefaultOptions (options) {
+		function defaultTrue(value) {
+			return value === undefined ? true : (value || false);
+		}
+		options.width = parseInt(options.width || css.call(this.dom, "width") || 600);
+		options.height = parseInt(options.height || css.call(this.dom, "height") || 400);
+		options.autoUpdate = defaultTrue(options.autoUpdate);
+		options.colors = options.colors || ["red", "blue", "green"];
+		options.reverseData = options.reverseData || false;
+		options.marker = options.marker || "circle";
+		options.max = options.max || 1;
+		options.yMarkerWidth = parseInt(options.yMarkerWidth || 100);
+		options.shift = defaultTrue(options.shift);
+		options.dataLength = options.dataLength || 20;
+		return options;
 	}
 
 	/**
@@ -53,44 +109,106 @@
 	_Graph = function (domId, data, options) {
 		var dom = _(domId) || __(domId),
 			canvas,
-			ctx;
+			ctx,
+			checkType, width, height,
+			excanvas;
 
 		if (!dom) {
 			alert("Wrong dom wrapper");
 			return;
 		}
 
-		canvas = _create("canvas");
-		if (!canvas.getContext) {
-			/**
-			 * 	TODO : import ExCanvas.js
-			 */
+		if (dom.tagName.toLowerCase() === "canvas") {
+			canvas = dom;
+		} else {
+			canvas = _create("canvas");
+			if (!canvas.getContext) {
+				/**
+				 * 	TODO : import ExCanvas.js
+				 */
+				 excanvas = document.createElement("script");
+
+			}
+			dom.appendChild(canvas);
 		}
 		ctx = canvas.getContext("2d");
 
 		this.dom = dom;
 		this.canvas = canvas;
 		this.ctx = ctx;
-		if (data instanceof Array)
 		this.data = data || [];
-		this.options = options || {};
+		checkType = this.data[0];
+		if (checkType !== undefined && !(checkType instanceof Array)) {
+			this.data = [this.data];
+		}
+
+		this.options = setDefaultOptions.call(this, options || {});
+
+		width = this.options.width;
+		height = this.options.height;
+
+		canvas.width = width;
+		canvas.height = height;
+		dom.style.width = width + "px";
+		dom.style.height = height + "px";
 
 		this.render();
 	};
-	_Graph.prototype.appendData = function (data) {
-		var data = this.data;
-		if (this.options.shift && data.length > 0) {
-			data.splice(0, 1);
+
+	_Graph.prototype.appendData = function (data, updateGraph) {
+		var data = this.data,
+			dataLength = data.length,
+			options = this.options,
+			i, row;
+		if (options.reverseData) {
+			data.push(data);
+			if (options.shift && dataLength > options.dataLength) {
+				data.splice(0, 1);
+			}
+		} else {
+			for (i = 0; i < dataLength; i++) {
+				row = data[i];
+				if (options.shift && dataLength > options.dataLength) {
+					row.splice(0, 1);
+				}
+			}
 		}
-		data.push(data);
-		if (this.options.autoUpdate) {
+		if (updateGraph || (updateGraph === undefined && this.options.autoUpdate)) {
 			this.render();
 		}
 	};
+
 	_Graph.prototype.render = function () {
-		var data = this.data;
+		var data = this.data,
+			dataLength = data.length,
+			options = this.options,
+			width = options.width,
+			height = options.height,
+			ctx = this.ctx,
+			renderingData,
+			renderingDataLength,
+			i, j, tickWidth,
+			yMarkerWidth = options.yMarkerWidth,
+			max = options.max,
+			heightRatio = height / max;
+
+		for (i = dataLength; i--;) {
+			renderingData = data[i];
+			renderingDataLength = renderingData.length;
+			if (renderingDataLength > 0) {
+				tickWidth = (width - yMarkerWidth) / renderingDataLength;
+				ctx.strokeStyle = options.colors[i];	//TODO : check color index
+				ctx.beginPath();
+				ctx.moveTo(yMarkerWidth, heightRatio * renderingData[0]);
+
+				for (j = 1; j < renderingDataLength; j++) {
+					ctx.lineTo(yMarkerWidth + tickWidth * j, heightRatio * renderingData[j]);
+				}
+				ctx.stroke();
+			}
+		}
 	};
 
 
 	win.VJ = VJ;
-}(document, window, VJ));
+}(document, window, window.VJ));
